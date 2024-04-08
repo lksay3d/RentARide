b915180d6609e770d8d215b4cbb8769d36b1dd34
brooklyn little <brooklit@umich.edu>
2024-04-08 02:23:21 -0400

    added welcom screen + edited some others

---
+++ welcome_script.js
@@ -0,0 +1,28 @@
+const menuToggle = document.querySelector('.toggle');
+      const showcase = document.querySelector('.showcase');
+
+      menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+        showcase.classList.toggle('active');
+      })
+
+
+document.getElementById("explore-button").addEventListener("click", function(){
+    window.location.href = "booking_index.html";
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the login link element
+  var loginLink = document.getElementById("login");
+
+  // Attach a click event listener to the login link
+  loginLink.addEventListener("click", function(event) {
+      // Prevent the default action of following the link
+      event.preventDefault();
+
+      // Open the desired page
+      window.location.href = "your_page_url.html";
+  });
+});
+
+
